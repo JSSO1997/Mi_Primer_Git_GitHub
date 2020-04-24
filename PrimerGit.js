a9768cfd9bb8a7204c610b88466cb42830efbd76
Nikumi <jsebastiansilvao@gmail.com>
2020-04-24 18:49:24 -0500

    Etiqueta HTML y Texto en JS

--- PrimerGit.js
+++ PrimerGit.js
@@ -1,2 +1,4 @@
 document.getElementById("Titulo").innerHTML="¡Hey!, esta es mi primera\
  página con repositorios Git"
+ document.getElementById("Titulo").innerHTML="Oye estoy probando lo que son los\
+  commits para guardar cambios de programación con versiones"
